390538eff5074cf9abccebda7d9699e63656d5ff
107061217 <1067pr@gamil.com>
2020-04-14 22:45:36 +0800

    first commit

---
+++ FFT.py
@@ -0,0 +1,46 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import serial
+import time
+Fs=100.0; # sampling rate
+Ts=10.0/Fs; # sampling interval
+t = np.arange(0,10,Ts) # time vector; create Fs samples between 0 and 10 sec.
+x = np.arange(0,10,Ts) # signal vector; create Fs samples
+y = np.arange(0,10,Ts) # signal vector; create Fs samples
+z = np.arange(0,10,Ts) # signal vector; create Fs samples
+tilt = np.arange(0,10,Ts) # signal vector; create Fs samples
+
+serdev = '/dev/ttyACM0'
+s = serial.Serial(serdev,115200)
+for n in range(0, int(Fs)):
+    line=s.readline() # Read an echo string from K66F terminated with '\n'
+    # print line
+    x[n] = float(line)
+    line=s.readline() # Read an echo string from K66F terminated with '\n'
+    # print line
+    y[n] = float(line)
+    line=s.readline() # Read an echo string from K66F terminated with '\n'
+    # print line
+    z[n] = float(line)
+
+for n in range(0, 100):
+    if (x[n] < -0.5):
+        tilt[n] = 1.0
+    else :
+        tilt[n] = 0
+
+fig, ax = plt.subplots(2, 1)
+ax[0].plot(t,x, color = "blue",  label = "x")
+ax[0].plot(t,y, color = "red",   label = "y")
+ax[0].plot(t,z, color = "green",  label = "z")
+ax[0].legend(loc='lower left', frameon=True)
+ax[0].set_xlabel('Time')
+ax[0].set_ylabel('Acc Vector')
+for i in range(0, 100) :
+    ax[1].plot([i/10, i/10], [0, tilt[i]], color = 'blue') 
+    ax[1].scatter([i/10,], [tilt[i],], 10, color = 'blue')
+ax[1].plot([0, 10], [0, 0], color = "red")
+ax[1].set_xlabel('Time')
+ax[1].set_ylabel('Tilt')
+plt.show()
+s.close()
---
+++ main.cpp
@@ -0,0 +1,134 @@
+#include "mbed.h"
+#include "fsl_port.h"
+#include "fsl_gpio.h"
+#include "mbed_events.h"
+
+#define UINT14_MAX        16383
+// FXOS8700CQ I2C address
+#define FXOS8700CQ_SLAVE_ADDR0 (0x1E<<1) // with pins SA0=0, SA1=0
+#define FXOS8700CQ_SLAVE_ADDR1 (0x1D<<1) // with pins SA0=1, SA1=0
+#define FXOS8700CQ_SLAVE_ADDR2 (0x1C<<1) // with pins SA0=0, SA1=1
+#define FXOS8700CQ_SLAVE_ADDR3 (0x1F<<1) // with pins SA0=1, SA1=1
+// FXOS8700CQ internal register addresses
+#define FXOS8700Q_STATUS 0x00
+#define FXOS8700Q_OUT_X_MSB 0x01
+#define FXOS8700Q_OUT_Y_MSB 0x03
+#define FXOS8700Q_OUT_Z_MSB 0x05
+#define FXOS8700Q_M_OUT_X_MSB 0x33
+#define FXOS8700Q_M_OUT_Y_MSB 0x35
+#define FXOS8700Q_M_OUT_Z_MSB 0x37
+#define FXOS8700Q_WHOAMI 0x0D
+#define FXOS8700Q_XYZ_DATA_CFG 0x0E
+#define FXOS8700Q_CTRL_REG1 0x2A
+#define FXOS8700Q_M_CTRL_REG1 0x5B
+#define FXOS8700Q_M_CTRL_REG2 0x5C
+#define FXOS8700Q_WHOAMI_VAL 0xC7
+
+DigitalOut led1(LED1);
+InterruptIn sw2(SW2);
+EventQueue q1;
+Thread t1;
+EventQueue q2;
+Thread t2;
+
+I2C i2c( PTD9,PTD8);
+Serial pc(USBTX, USBRX);
+int m_addr = FXOS8700CQ_SLAVE_ADDR1;
+
+void FXOS8700CQ_readRegs(int addr, uint8_t * data, int len);
+void FXOS8700CQ_writeRegs(uint8_t * data, int len);
+void acc(float * t, int16_t acc16, uint8_t * res);
+void blink();
+void que_acc(float * t, int16_t acc16, uint8_t * res);
+void que_blink();
+
+int main() {
+
+   pc.baud(115200);
+
+   uint8_t who_am_i, data[2], res[6];
+   int16_t acc16;
+   float t[3];
+
+   led1 = 1;
+
+   // Enable the FXOS8700Q
+
+   FXOS8700CQ_readRegs( FXOS8700Q_CTRL_REG1, &data[1], 1);
+   data[1] |= 0x01;
+   data[0] = FXOS8700Q_CTRL_REG1;
+   FXOS8700CQ_writeRegs(data, 2);
+
+   // Get the slave address
+   FXOS8700CQ_readRegs(FXOS8700Q_WHOAMI, &who_am_i, 1);
+
+   // pc.printf("Here is %x\r\n", who_am_i);
+   
+   t1.start(callback(&q1, &EventQueue::dispatch_forever));
+   t2.start(callback(&q2, &EventQueue::dispatch_forever));
+   
+   sw2.rise(q2.event(&que_blink));
+   sw2.fall(q1.event(&que_acc, t, acc16, &res[1]));
+   
+}
+
+void acc(float * t, int16_t acc16, uint8_t * res) {
+   int num = 0;
+   float data[100][3];
+
+   while (num < 100) {
+
+      FXOS8700CQ_readRegs(FXOS8700Q_OUT_X_MSB, res, 6);
+
+      acc16 = (res[0] << 6) | (res[1] >> 2);
+      if (acc16 > UINT14_MAX/2)
+         acc16 -= UINT14_MAX;
+      t[0] = ((float)acc16) / 4096.0f;
+
+      acc16 = (res[2] << 6) | (res[3] >> 2);
+      if (acc16 > UINT14_MAX/2)
+         acc16 -= UINT14_MAX;
+      t[1] = ((float)acc16) / 4096.0f;
+
+      acc16 = (res[4] << 6) | (res[5] >> 2);
+      if (acc16 > UINT14_MAX/2)
+         acc16 -= UINT14_MAX;
+      t[2] = ((float)acc16) / 4096.0f;
+
+      data[num][0] = t[0];
+      data[num][1] = t[1];
+      data[num][2] = t[2];
+      num++;
+      wait(0.1);  
+   }
+   num = 0;
+   while (num < 100) {
+      pc.printf("%1.4f\r\n%1.4f\r\n%1.4f\r\n", data[num][0], data[num][1], data[num][2]);
+      num++;
+      wait(0.05);
+   }
+
+}
+
+void blink() { 
+   for (int t=0;t<10;t++) {
+      led1 = !led1;
+      wait(1);     
+   }
+}
+
+void que_acc(float * t, int16_t acc16, uint8_t * res) {
+   q1.call(&acc, t, acc16, res);
+}
+void que_blink() {
+   q2.call(&blink);
+}
+void FXOS8700CQ_readRegs(int addr, uint8_t * data, int len) {
+   char t = addr;
+   i2c.write(m_addr, &t, 1, true);
+   i2c.read(m_addr, (char *)data, len);
+}
+
+void FXOS8700CQ_writeRegs(uint8_t * data, int len) {
+   i2c.write(m_addr, (char *)data, len);
+}
